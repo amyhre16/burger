b93610bf3d81cdd6741c4b25c4d55c72b0c1eed2
Anthony Myhre <anthony.myhre@utexas.edu>
2017-02-19 19:10:20 -0600

    app is working locally going to push to heroku

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,54 +1,25 @@
 'use strict';
 
-// var express = require('express');
 var burger = require('./../models/burger.js');
 
 module.exports = function(app) {
 	app.get('/', function(req, res) {
-		// var burgers = burger.selectAll(consoleLog);
-		/*console.log("burgers");
-		console.log(burgers);*/
-		/*var burgersArr = [
-			{
-				id: 1,
-				burger_name: "blt",
-				devoured: 0
-			},
-			{
-				id: 2,
-				burger_name: "cheese",
-				devoured:0
-			},
-			{
-				id: 3,
-				burger_name: "bbq",
-				devoured: 1
-			}
-		];*/
-		// var burgersArr = burger.selectAll(consoleLog);
-		// console.log(burgersArr);
-		// res.render('index', {burgers: burger.selectAll(consoleLog)});
 		burger.selectAll(res, renderPage);
-		// res.render('index', {burgers: burger.selectAll(consoleLog, res)});
-		// res.json(burger.selectAll());
 	});
 
-	app.post('/insertOne', function(req, res) {
-		burger.insertOne(req.body.burger_name, res, renderUpdatedDB);
+	app.post('/', function(req, res) {
+		burger.insertOne(req.body.burger_name, res, redirectPage);
 	});
 
-	app.post('/updateOne', function(req, res) {
-		burger.updateOne(req.body.id, 1, res, renderUpdatedDB);
+	app.put('/', function(req, res) {
+		burger.updateOne(req.body.id, 1, res, redirectPage);
 	});
 };
 
 function renderPage(result, res) {
-	/*console.log("result");
-	console.log(result);*/
-	// return result;
 	res.render('index', {burgers: result});
 }
 
-function renderUpdatedDB(res) {
-	burger.selectAll(res, renderPage);
+function redirectPage(res) {
+	res.redirect('/');
 }
