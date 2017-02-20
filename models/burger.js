aad807d21db7cfd9fdd89665a55c6e562d50ed66
Anthony Myhre <anthony.myhre@utexas.edu>
2017-02-19 23:37:00 -0600

    removed commented out code added comments to code

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,27 @@
+// import orm.js
 var orm = require('./../config/orm.js');
 
-var burger = {
+// exports to burgers_controller.js
+module.exports = {
+	// takes in res from ajax request and a callback function as parameters
 	selectAll: function(res, callback) {
-		// console.log("burger.js");
-		// console.log(orm.selectAll("burgers"));
-		var burger = orm.selectAll("burgers", res, callback);
-		// console.log(burger);
-		// return burger;
-		// return orm.selectAll("burgers");
+		// run the select all query from orm.js and pass in "burgers" to the table parameter, res and the callback function
+		orm.selectAll("burgers", res, callback);
 	},
+	// takes in the burgername and res from ajax request and callback function as parameters
 	insertOne: function (burger_name, res, callback) {
-		return orm.insertOne("burgers", "burger_name", burger_name, res, callback);
+		/*
+			run the insertOne query from orm.js
+			pass in parameters "burgers" for table name, "burger_name" for column name, burger_name for the value, res from ajax request and callback function
+		*/
+		orm.insertOne("burgers", "burger_name", burger_name, res, callback);
 	},
+	// takes in id and res from ajax request and callback function as parameters
 	updateOne: function(id, devoured, res, callback) {
-		return orm.updateOne("burgers", {devoured: devoured}, {id: id}, res, callback);
+		/*
+			run update query from orm.js
+			pass in "burgers" for table name, devoured obj for the set clause, id object for where clause, res from ajax request and callback function
+		*/
+		orm.updateOne("burgers", {devoured: devoured}, {id: id}, res, callback);
 	}
 };
-
-module.exports = burger;
